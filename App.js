202b388edce42e08c66625ffaaa67972c6d59450
Oscar Olsson <69190482+oscarolsson94@users.noreply.github.com>
2022-11-07 15:08:51 +0100

    adding iconbutton

--- App.js
+++ App.js
@@ -1,18 +1,33 @@
 import { StatusBar } from "expo-status-bar";
-import { NavigationContainer } from "@react-navigation/native";
+import { NavigationContainer, useNavigation } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { AllPlaces } from "./screens/AllPlaces";
 import { AddPlace } from "./screens/Addplace";
+import { IconButton } from "./components/UI/IconButton";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+  const { navigate } = useNavigation();
   return (
     <>
       <StatusBar style="dark" />
       <NavigationContainer>
         <Stack.Navigator>
-          <Stack.Screen name="AllPlaces" component={AllPlaces} />
+          <Stack.Screen
+            name="AllPlaces"
+            component={AllPlaces}
+            options={{
+              headerRight: ({ tintColor }) => (
+                <IconButton
+                  icon="add"
+                  size={24}
+                  color={tintColor}
+                  onPress={() => navigate("AddPlace")}
+                />
+              ),
+            }}
+          />
           <Stack.Screen name="AddPlace" component={AddPlace} />
         </Stack.Navigator>
       </NavigationContainer>
